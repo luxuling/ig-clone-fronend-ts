ac23137b3483067286c3db0294003c2cfa9a64e2
lixuling <antonisaputra049@gmail.com>
2023-06-05 05:07:51 +0700

    bug fix

--- src/layouts/UniversalLayout.tsx
+++ src/layouts/UniversalLayout.tsx
@@ -1,14 +1,12 @@
 import BottomNavbar from '@components/navbar/BottomNavbar';
-import HomeNavbar from '@components/navbar/HomeNavbar';
 import * as React from 'react';
 
 interface IHomeLayout {
   children: React.ReactElement;
 }
-export default function HomeLayout({ children }: IHomeLayout) {
+export default function UniversalLayout({ children }: IHomeLayout) {
   return (
     <div className="bg-black py-[60px] md:py-0 md:flex md:flex-row-reverse md:justify-between md:relative">
-      <HomeNavbar />
       {children}
       <BottomNavbar />
     </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,25 @@
 import withAuth from '@components/hoc/withAuth';
+import HomeNavbar from '@components/navbar/HomeNavbar';
 import ShowAllPost from '@components/sections/show-all-post';
 import Suggestion from '@components/sections/show-suggestion';
 import StoryAccountSlider from '@components/slider/story';
-import HomeLayout from 'layouts/HomeLayout';
+import UniversalLayout from 'layouts/UniversalLayout';
 import React from 'react';
 
 export function Home() {
   return (
-    <HomeLayout>
-      <div className="lg:flex lg:flex-1">
-        <div className="md:flex-1 md:w-[697px]">
-          <StoryAccountSlider />
-          <ShowAllPost />
+    <UniversalLayout>
+      <>
+        <HomeNavbar />
+        <div className="lg:flex lg:flex-1">
+          <div className="md:flex-1 md:w-[697px]">
+            <StoryAccountSlider />
+            <ShowAllPost />
+          </div>
+          <Suggestion />
         </div>
-        <Suggestion />
-      </div>
-    </HomeLayout>
+      </>
+    </UniversalLayout>
   );
 }
 export default withAuth(Home);
